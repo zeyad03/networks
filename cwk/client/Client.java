0f38c3695fc76776ca01b998abf3f32c853a6172
zeyad03 <sc22zb@leeds.ac.uk>
2024-03-17 17:23:51 +0000

    creating a simple connection between server and client on port 4242

--- cwk/client/Client.java
+++ cwk/client/Client.java
@@ -1,6 +1,35 @@
+import java.io.*;
+import java.net.*;
+
 public class Client 
 {
+	public void connect()
+	{
+		try
+		{
+			Socket s = new Socket("localhost", 4242);
+
+			// Buffer the input strem for performance.
+			BufferedReader reader = new BufferedReader(
+															new InputStreamReader(
+																s.getInputStream()
+															));
+			String msg = reader.readLine();
+			System.out.println("Action: " + msg);
+
+			// Close reader and connection.
+			reader.close();
+			s.close();
+		}
+		catch( IOException e )
+		{
+			System.out.println(e);
+		}
+	}
+
 	public static void main( String[] args )
 	{
+		Client client = new Client();
+		client.connect();
 	}
 }
--- cwk/server/Server.java
+++ cwk/server/Server.java
@@ -1,6 +1,44 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Server 
 {
+
+	public void runServer()
+	{
+		try
+		{
+			// For the example in lectures, we use port 4242.
+			ServerSocket serverSock = new ServerSocket(4242);
+
+			while( true )
+			{
+				// Accept; blocking; will not return until a client has made contact.
+				Socket sock = serverSock.accept();
+
+				// Get information about the connection.
+				InetAddress inet = sock.getInetAddress();
+				Date date = new Date();
+				System.out.println("\nDate " + date.toString());
+				System.out.println("Connection made from " + inet.getCanonicalHostName());
+
+				// Imitate uploading files.
+				PrintWriter writer = new PrintWriter(sock.getOutputStream());
+				writer.println("File has been uploaded.\n");
+				writer.close();
+				sock.close();
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+
 	public static void main( String[] args )
 	{
+		Server server = new Server();
+		server.runServer();
 	}
 }
